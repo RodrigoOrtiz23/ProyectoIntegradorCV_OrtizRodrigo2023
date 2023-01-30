bb382a4e0641b6130ceb78b7e2878151a0b74b4f
RodrigoOrtiz23 <123128174+RodrigoOrtiz23@users.noreply.github.com>
2023-01-30 18:31:38 -0300

    Se termina de completar el CV.

--- JS/Main.js
+++ JS/Main.js
@@ -3,6 +3,7 @@ const nombre = document.getElementById('nombre');
 const telefono = document.getElementById('telefono');
 const email = document.getElementById('email');
 const direccion = document.getElementById('direccion');
+const html = document.getElementById('html');
 
 const Cargadeperfil = async () => {
     try {
@@ -10,7 +11,6 @@ const Cargadeperfil = async () => {
         const repuesta = await fetch(url);
         const { results } = await repuesta.json();
         const datos = results[0];
-        console.log(datos);
         foto.src = datos.picture.medium;
         nombre.textContent = datos.name.first+" "+datos.name.last;
         telefono.textContent = datos.cell;
