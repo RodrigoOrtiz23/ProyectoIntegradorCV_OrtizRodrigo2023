d019767e23fa1cecd61781033482450751bea72d
RodrigoOrtiz23 <123128174+RodrigoOrtiz23@users.noreply.github.com>
2023-01-29 22:23:50 -0300

    Se completa bloque de informacion complementaria

--- JS/Main.js
+++ JS/Main.js
@@ -1,6 +1,8 @@
 const foto = document.getElementById('foto');
 const nombre = document.getElementById('nombre');
-const apellido = document.getElementById('apellido');
+const telefono = document.getElementById('telefono');
+const email = document.getElementById('email');
+const direccion = document.getElementById('direccion');
 
 const Cargadeperfil = async () => {
     try {
@@ -10,8 +12,10 @@ const Cargadeperfil = async () => {
         const datos = results[0];
         console.log(datos);
         foto.src = datos.picture.medium;
-        nombre.textContent = datos.name.first;
-        apellido.textContent = datos.name.last;
+        nombre.textContent = datos.name.first+" "+datos.name.last;
+        telefono.textContent = datos.cell;
+        email.textContent = datos.email;
+        direccion.textContent = datos.location.country+", "+datos.location.city;
 
     } catch (error) {
         console.log(error);
